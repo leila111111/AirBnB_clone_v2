ec12a0df9f35b4ba8d6746c225996e149fb30d11
leila111111 <leila_merzak@outlook.fr>
2023-10-22 23:41:38 +0000

    tasks

---
+++ web_flask/9-states.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""script that starts a Flask web application:
+listening on 0.0.0.0, port 5000"""
+from flask import Flask
+from flask import render_template
+from models import storage
+from models.state import State
+
+app = Flask(__name__)
+
+
+@app.teardown_appcontext
+def teardown_app(exc):
+    """After each request you must remove the current SQLAlchemy Session"""
+    storage.close()
+
+
+@app.route("/states", strict_slashes=False)
+def states():
+    """display a HTML page: (inside the tag BODY)"""
+    states_list = storage.all(State)
+    return render_template("9-states.html", states=states_list)
+
+
+@app.route("/states/<id>", strict_slashes=False)
+def states_id():
+    """display a HTML page: (inside the tag BODY)"""
+    for states in storage.all("State").values():
+        if states.id == id:
+            return render_template("9-states.html", states=states)
+    return render_template("9-states.html")
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
