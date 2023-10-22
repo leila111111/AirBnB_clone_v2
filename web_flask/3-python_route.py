3c9762058d12be5b8dc39cf868d865553c1a6f76
leila111111 <leila_merzak@outlook.fr>
2023-10-22 20:34:26 +0000

    tassks

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -4,7 +4,7 @@ listening on 0.0.0.0, port 5000
 Routes:
 /: display “Hello HBNB!”
 /hbnb: display “HBNB”
-/c/<text>: display “C ”, followed by the value 
+/c/<text>: display “C ”, followed by the value
 of the text variable (replace underscore _ symbols with a space )
 /python/<text>: display “Python ”, followed by the value of the
 text variable (replace underscore _ symbols with a space )"""
